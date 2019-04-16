7b3ff9112d429b0b54d274254fbc8e687bad2d7f
jacheima <jacheima@uat.edu>
2019-04-15 20:03:30 -0700

    I created a unity project for the cooking game. I got the dry storage background in and got the door in as a button and the door opens and takes you to a new scene. I also watched a tutorial on how to make a hidden object game

---
+++ Cooking_Game/KitchenWars/Assets/FridgeDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FridgeDoor : MonoBehaviour {
+
+    public Sprite[] doorStates;
+
+    public Button openDoor;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+     
+	}
+
+    public void WalkInFridge()
+    {
+        if (openDoor.image.sprite == doorStates[0])
+        {
+            openDoor.image.sprite = doorStates[1];
+
+           
+        }
+
+        Debug.Log("The player has opened the fridge door");
+
+        if (openDoor.image.sprite == doorStates[1])
+        {
+            SceneManager.LoadScene("WalkInFridge");
+        }
+
+    }
+}
